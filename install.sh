5d47d65dc39f349cdf98fbb7b474ff249b42b46b
MAX <max.bu@ubnt.com>
2018-07-12 11:32:06 +0800

    Add add_rules, del_rules, backup_iptables, restore_iptables funcs

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Maintainer MMX 
 # Email 4isnothing@gmail.com
+add_rules()
+{
 curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > cn_ipv4.list
 wait
 
@@ -9,10 +11,29 @@ iptables -t nat -N BYPASSLIST
 
 for ip in $(cat cn_ipv4.list)
 do
+  echo "iptables -t nat -A BYPASSLIST -d $ip -j RETURN"
   echo "iptables -t nat -A BYPASSLIST -d $ip -j RETURN" >> iptables_bypass.sh
   iptables -t nat -A BYPASSLIST -d $ip -j RETURN
 done
 
 echo "Finished"
+}
 
+del_rules()
+{
+iptables -t nat -F BYPASSLIST
+iptables -t nat -X BYPASSLIST
+}
+
+backup_iptables()
+{
+iptables-save > iptables.conf
+}
+
+restore_iptables()
+{
+iptables-restore < iptables.conf
+}
+
+"$@"
 
