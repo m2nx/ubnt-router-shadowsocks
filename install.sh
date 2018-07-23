2800e25aff4a5d5928fbc976eb1d61689c108b32
MMX <max.bu@ubnt.com>
2018-07-23 16:12:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,17 +45,10 @@ iptables -t nat -A $CHAIN_NAME -d 240.0.0.0/4 -j RETURN
 iptables -t nat -A $CHAIN_NAME -p tcp -m set --match-set chnlist dst -j RETURN
 
 # redirect
-iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports 1080
+# iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports 1080
 
 # Add to prerouting chain
 iptables -t nat -A PREROUTING -p tcp -j $CHAIN_NAME
-
-#for ip in $(cat cn_ipv4.list)
-#do
-#  echo "iptables -t nat -A $CHAIN_NAME -d $ip -j RETURN"
-#  echo "iptables -t nat -A $CHAIN_NAME -d $ip -j RETURN" >> iptables_bypass.sh
-#  iptables -t nat -A $CHAIN_NAME -d $ip -j RETURN
-#done
 
 echo 'Done.'
 }
